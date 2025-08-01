7227bf2144e16b7746e3d842d1ee57d25478727a
fgmonk <felipegonzalezmat@gmail.com>
2025-07-31 22:18:11 -0400

    Eliminado código de prueba, dejado configuración SMTP para envío de correos

--- sistema_psicologas/settings.py
+++ sistema_psicologas/settings.py
@@ -131,3 +131,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
 LOGIN_URL = '/accounts/login/'
+
+# Configuración de correo para Gmail (envío de emails)
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'sistemapsicologas@gmail.com'
+EMAIL_HOST_PASSWORD = 'vsna pryf txlh xqxl'  # Contraseña de aplicación generada en Gmail
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
